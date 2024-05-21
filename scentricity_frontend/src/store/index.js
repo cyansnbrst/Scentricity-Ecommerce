d5d79836d91925661dafc66f3e42f2c6c674a581
Ekaterina <litra1122@gmail.com>
2024-05-21 16:48:28 +0300

    minor bugs fixed

--- scentricity_frontend/src/store/index.js
+++ scentricity_frontend/src/store/index.js
@@ -2,7 +2,7 @@ import {createStore} from 'vuex';
 
 export default createStore({
     state: {
-        backendURL: '176.109.99.199:8000/api/v1',
+        backendURL: 'http://176.109.99.199:8000/api/v1',
         authToken: null,
     },
     mutations: {
